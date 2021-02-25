702a26c3ab1f9c84d13487a2677cb8e4fad7239e
evoworld1 <evoworld1@gmail.com>
2021-02-25 17:50:22 +0100

    add basic functionalities, change constructor function to class

--- index.js
+++ index.js
@@ -1,36 +1,90 @@
-const addButton = document.querySelector(".add-btn");
+const enterButton = document.querySelector(".enter");
 const intro = document.querySelector(".intro");
 const formPopUp = document.querySelector(".form-popUp");
+const addButton = document.querySelector(".add-btn");
+const bookContainer = document.querySelector(".bookContainer");
+const requestFormButton = document.querySelector(".request-form");
 
-(function addBook() {
-  addButton.addEventListener("click", () => {
-    event.preventDefault();
-    intro.classList.add("fadeOut");
-    setTimeout(formAnimation, 500);
-    function formAnimation() {
-      formPopUp.classList.add("fadeIn");
-    }
-  });
-})();
-
-function libraryCheck() {}
-
-libraryCheck.prototype.showInfo = function () {
-  return this.title + this.author + this.pages + this.readOrNot;
-};
-
-function library(title, author, pages, readOrNot) {
-  //that's the constructor function
-  this.title = title;
-  this.author = author;
-  this.pages = pages;
-  this.readOrNot = readOrNot;
+class bookData {
+  constructor(title, author, pages, read) {
+    this.title = form.title.value;
+    this.author = form.author.value;
+    this.pages = form.pages.value;
+    this.read = form.read.checked;
+  }
 }
 
-library.prototype = Object.create(libraryCheck.prototype);
+let booksArray = [];
+let newBook;
 
-const book = new library("Boska cząstka", "L. Lederman", 500, "not read");
+function addBookNode() {
+  newBook = new bookData(title, author, pages, read);
+  booksArray.push(newBook);
+  for (let i = 0; i < booksArray.length; i++) {
+    createBookNode(booksArray[i]);
+  }
+}
 
-book.showInfo();
+function createBookNode(data) {
+  const bookDiv = document.createElement("div");
+  bookDiv.setAttribute("id", `bookNode`);
+  bookDiv.classList.add("bookNode");
+  bookContainer.appendChild(bookDiv);
 
-console.log(book.showInfo());
+  const titleDiv = document.createElement("div");
+  titleDiv.classList.add("bookTitle");
+  titleDiv.textContent = data.title;
+  bookDiv.appendChild(titleDiv);
+
+  const authorDiv = document.createElement("div");
+  authorDiv.classList.add("bookAuthor");
+  authorDiv.textContent = data.author;
+  bookDiv.appendChild(authorDiv);
+
+  const pagesDiv = document.createElement("div");
+  pagesDiv.classList.add("bookPages");
+  pagesDiv.textContent = data.pages;
+  bookDiv.appendChild(pagesDiv);
+}
+
+//Events for animations
+enterButton.addEventListener("click", () => {
+  event.preventDefault();
+  intro.classList.add("fadeOut");
+  setTimeout(formAnimation, 500);
+  function formAnimation() {
+    formPopUp.classList.add("fadeIn");
+  }
+});
+
+requestFormButton.addEventListener("click", () => {
+  event.preventDefault();
+  bookContainer.classList.remove("fadeIn");
+  bookContainer.classList.add("fadeOut");
+  setTimeout(formAnimation, 500);
+  function formAnimation() {
+    formPopUp.classList.remove("fadeOut");
+    formPopUp.classList.add("fadeIn");
+  }
+});
+
+addButton.addEventListener("click", () => {
+  event.preventDefault();
+  addBookNode();
+  form.reset();
+  formPopUp.classList.remove("fadeIn");
+  formPopUp.classList.add("fadeOut");
+  setTimeout(formAnimation, 500);
+  function formAnimation() {
+    bookContainer.classList.remove("fadeOut");
+    bookContainer.classList.add("fadeIn");
+  }
+});
+
+//check TEST ONLY!
+
+const checkBtn = document.querySelector(".check-array");
+
+checkBtn.addEventListener("click", () => {
+  console.log(booksArray);
+});
